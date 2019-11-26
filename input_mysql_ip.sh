eaa4ff605e442e6a6f04e91c840c551abf7eccba
learning2016 <1330107395@qq.com>
2019-11-26 18:11:22 +0800

    Update input_mysql_ip.sh

--- input_mysql_ip.sh
+++ input_mysql_ip.sh
@@ -3,4 +3,4 @@ read -p "请输入Mysql的IP:" ip
 echo "$ip"
 i1="ip="
 i2=$i1$ip
-sed "1 a$i2" -i /etc/zabbix/etc/zabbix/zabbix_agentd.d/nmap_mysql.sh
+sed "1 a$i2" -i /etc/zabbix/zabbix_agentd.d/nmap_mysql.sh
