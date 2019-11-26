a017571c2e4c02fba2bf4a1f976f05f86b407ec3
learning2016 <1330107395@qq.com>
2019-11-26 18:11:40 +0800

    Update input_memcache_ip.sh

--- input_memcache_ip.sh
+++ input_memcache_ip.sh
@@ -3,4 +3,4 @@ read -p "请输入Memcache的IP:" ip
 echo "$ip"
 i1="ip="
 i2=$i1$ip
-sed "1 a$i2" -i /etc/zabbix/etc/zabbix/zabbix_agentd.d/nmap_memcache.sh
+sed "1 a$i2" -i /etc/zabbix/zabbix_agentd.d/nmap_memcache.sh
