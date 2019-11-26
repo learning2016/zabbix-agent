6ae7b74ce373b0990c847137f25471e814578a1c
learning2016 <1330107395@qq.com>
2019-11-26 18:12:13 +0800

    Update input_redis_ip.sh

--- input_redis_ip.sh
+++ input_redis_ip.sh
@@ -3,4 +3,4 @@ read -p "请输入Redis的IP:" ip
 echo "$ip"
 i1="ip="
 i2=$i1$ip
-sed "1 a$i2" -i /etc/zabbix/etc/zabbix/zabbix_agentd.d/nmap_redis.sh
+sed "1 a$i2" -i /etc/zabbix/zabbix_agentd.d/nmap_redis.sh
