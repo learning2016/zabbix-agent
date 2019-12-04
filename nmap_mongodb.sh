12a4472f386262935b1ef16eccd63d3c103af204
learning2016 <1330107395@qq.com>
2019-12-04 18:26:14 +0800

    Update nmap_mongodb.sh

--- nmap_mongodb.sh
+++ nmap_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-namp=`nmap $ip -p 27017 | grep open | wc -l`
+namp=`nmap $1 -p $2 | grep open | wc -l`
 if [ "$namp" == 1 ];then
-        echo 0
-else
         echo 1
+else
+        echo 0
 fi
